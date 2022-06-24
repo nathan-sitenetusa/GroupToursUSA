7dd689e0f45f9b8dc825b20588dbbc679adc3b8b
Matt <mtkloepf@mtu.edu>
2022-06-23 20:23:08 -0400

    Added RecyclerView content example data

--- app/src/main/java/com/example/grouptoursusa/MainActivity.kt
+++ app/src/main/java/com/example/grouptoursusa/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.grouptoursusa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.grouptoursusa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val contacts: List<String> = listOf("Nathan", "Matt", "Mikkel", "Kevin", "Ebare", "Byron")
+        val recyclerView = findViewById<RecyclerView>(R.id.contactsView)
+        recyclerView.adapter = ContactAdapter(this, contacts)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fun dont() {
+            print("We have saved humanity")
+        }
+        fun dontnot() {
+            print("We're screwed")
+        }
+
+        val goingToCrash : Boolean = true
+        if(goingToCrash)
+        {
+            dont()
+        }
+        else {
+            dontnot()
+        }
     }
 }
