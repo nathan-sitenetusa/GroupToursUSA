e301c1a4c45ab72dc0f2970287cffd4f2378a966
Matt <mtkloepf@mtu.edu>
2022-07-12 21:18:36 -0400

    Updated visuals for main screen and added checkboxes

--- app/src/main/java/com/example/grouptoursusa/MainActivity.kt
+++ app/src/main/java/com/example/grouptoursusa/MainActivity.kt
@@ -2,12 +2,6 @@ package com.example.grouptoursusa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.grouptoursusa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val contacts: List<String> = listOf("Nathan", "Matt", "Mikkel", "Kevin", "Ebare", "Byron")
-//        val recyclerView = findViewById<RecyclerView>(R.id.contactsView)
-//        recyclerView.adapter = ContactAdapter(this, contacts)
-//        recyclerView.setHasFixedSize(true)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
 //    private val navHostFragment = supportFragmentManager.findFragmentById(R.id.groupFragment) as NavHostFragment
